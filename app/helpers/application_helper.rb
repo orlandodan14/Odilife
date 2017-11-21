e6c071b8cab1b7effa9e578fa96b7c629a10d4d7
Orlando <orlando.dan14@gmail.com>
2017-11-21 08:36:48 -0300

    flavored ruby title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+	# Returns a full title on a per-pages basis.
+	def full_title(page_title = " ")
+		base_title = "Odilife"
+		if page_title.empty?
+			base_title
+		else
+			base_title + " | " + page_title
+		end
+	end
 end
